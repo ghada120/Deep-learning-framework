99fa7a0c57f4603eb783d28b307de6b2117d2c37
Ahmed.M.Ibrahim <35659429+am305949@users.noreply.github.com>
2021-01-24 22:20:09 +0200

    Update xor_fc.py

--- xor_fc.py
+++ xor_fc.py
@@ -26,11 +26,27 @@ error_per_epoch, accuracy_per_epoch = net.train(x_train, y_train, epochs=1000, l
 # test
 out = net.predict_output(x_train)
 out_decoded = label_decoder(out)
+label_decoded = list(np.squeeze(y_train))
 print(out, end="\n")
-print(out_decoded)
+print(out_decoded, end="\n")
+print(label_decoded, end="\n")
 
+
+# calculate confusion matrix, accuracy, precision, recall, f1 score
+conf_matrix = evaluation_metric(label_decoded, out_decoded, 'confusion matrix')
+accuracy = evaluation_metric(label_decoded, out_decoded, 'accuracy')
+precision = evaluation_metric(label_decoded, out_decoded, 'precision')
+recall = evaluation_metric(label_decoded, out_decoded, 'recall')
+f1 = evaluation_metric(label_decoded, out_decoded, 'f1')
+
+print("confusion matrix:")
+print(conf_matrix, end="\n")
+# print(conf_matrix.values)
+print("Total Accuracy: ", accuracy, end="\n")
+print("Total Precision: ", precision, end="\n")
+print("Total Recall: ", recall, end="\n")
+print("F1 score: ", f1, end="\n")
 
 # save model
 model = [x_train, y_train, out]
 save_model('D:/Projects/PyloXyloto/models', 'xor_fc', model)
-
