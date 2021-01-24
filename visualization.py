656fd9cb5aa590829f079d933a049a9e16ea705a
Ahmed.M.Ibrahim <35659429+am305949@users.noreply.github.com>
2021-01-24 20:43:57 +0200

    add show_image function

--- visualization.py
+++ visualization.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -31,7 +32,7 @@ def visualize(x, y):
     plt.close()
 
 
-def draw(x, y):
+def draw(x, y, x_label, y_label):
     """
         visualize the given data
         :param x: range of x axis
@@ -45,9 +46,8 @@ def draw(x, y):
         y_values = pd.DataFrame(y)
         plt.cla()
         plt.plot(x_values, y_values)
-        plt.xlabel('Epoch')
-        plt.ylabel('Error')
-        plt.title('Error per epoch')
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
         plt.gcf().autofmt_xdate()
         plt.tight_layout()
 
@@ -56,3 +56,27 @@ def draw(x, y):
     plt.tight_layout()
     plt.show()
     plt.close()
+
+
+def show_image(x_test, y_test, y_predicted):
+    """
+    show the images that we are predicting and the predicted value for it
+    :param x_test: data that we want to predict it
+    :param y_test: correct labels
+    :param y_predicted: predicted values
+    :return: images with their correct labels and predicted values
+    """
+    i = 0
+    for test, true in zip(x_test, y_test):
+        image = np.reshape(test, (28, 28))
+        plt.imshow(image, cmap='binary')
+        plt.show(block=False)
+        plt.pause(1)
+        plt.close()
+        pred = y_predicted[i]
+        idx = np.argmax(pred)
+        idx_true = np.argmax(true)
+        i += 1
+        print('pred: %s, prob: %.2f, true: %d' % (idx, pred[idx], idx_true))
+        # prob refers to the percentage of predicting this element (the most predicted value)
+
